b393c56d0feb8e87d4bd2d3d99c3ca9559bea8c7
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-07-08 19:44:41 +0530

    cors update again

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000','http://localhost:5500' , 'https://localhost:3443', 'http://127.0.0.1:3000', 'http://127.0.0.1:5000', 'http://127.0.0.1:5500','https://book-finder-dsp.herokuapp.com/','https://book-finder-dsp.netlify.app/about','https://bookfinder-frontend.herokuapp.com/'];
+const whitelist = ['http://localhost:3000','http://localhost:5500' , 'https://localhost:3443', 'http://127.0.0.1:3000', 'http://127.0.0.1:5000', 'http://127.0.0.1:5500','https://book-finder-dsp.herokuapp.com','https://book-finder-dsp.netlify.app','https://bookfinder-frontend.herokuapp.com'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
